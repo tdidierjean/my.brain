df2b7c701cabc88c726994c06ca7f8362dd074b8
tdidierjean <thomas.didierjean@gmail.com>
2010-08-17 17:09:15 +0200

    re-add config gile

---
+++ mybrain/config.php
@@ -0,0 +1,10 @@
+<?php 
+$CONFIG['dbType'] = 'mysql'; 
+$CONFIG['host'] = 'localhost';
+$CONFIG['user'] = 'myname';
+$CONFIG['password'] = 'mypassword';
+$CONFIG['db'] = 'mydbname';
+
+define('ROOT', dirname(__FILE__));
+
+?>
