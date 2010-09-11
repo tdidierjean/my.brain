300d0fd1312833e9109b6ea6ad594c31919879d6
tdidierjean <Thomas@.(none)>
2010-09-11 18:32:10 +0200

    fix login

--- mybrain/auth.php
+++ mybrain/auth.php
@@ -1,10 +1,9 @@
 <?php
 require_once('init.php');
 session_start();// get user info
-$user = $db->getUser($_REQUEST['username']);								
-/****************************************
-          Check password
-****************************************/
+$user = $db->getUser($_REQUEST['username']);				
+		
+// Check password
 if (!$user || (md5($_REQUEST['password']) != $user['password'])) {
 	header("Location: login.php?no_session=1");
 	exit();
