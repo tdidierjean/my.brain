e4e8dea827e0499e649bff0c95891a591cf2cf56
tdidierjean <thomas.didierjean@gmail.com>
2011-01-24 18:42:52 +0800

    fixed lucene path inclusion to work on all systems

--- mybrain/config.php
+++ mybrain/config.php
@@ -7,11 +7,15 @@ $CONFIG['db'] = 'artificiwbrain';
 
 // Get root path with '/' separators, even on windows
 if (DIRECTORY_SEPARATOR=='/')
-  $absolute_path = dirname(__FILE__);
+    $absolute_path = dirname(__FILE__);
 else
-  $absolute_path = str_replace('\\', '/', dirname(__FILE__));
-  
+    $absolute_path = str_replace('\\', '/', dirname(__FILE__));
+
 define('ROOT', $absolute_path);
 
 $CONFIG['indexPath'] = ROOT.'/docIndex';
+
+// Add lib folder to include path, necessary for Lucene
+$libPath = PATH_SEPARATOR . ROOT . '/lib/';
+set_include_path(get_include_path() . $libPath);
 ?>
